772f3bdedb8bcf52309c7d2e052eb5079cda5931
Justin Woo <wooj8@uci.edu>
2024-03-27 13:38:31 -0700

    displays categories of meals, unstyled

--- models/category.js
+++ models/category.js
@@ -1,9 +1,7 @@
-class Category {
+export default class Category {
   constructor(id, title, color) {
     this.id = id;
     this.title = title;
     this.color = color;
   }
 }
-
-export default Category;
